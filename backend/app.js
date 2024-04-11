6f1aa6a34b99fc93b0a2603cd6044246bd778119
Adii <115168602+yng-blood@users.noreply.github.com>
2024-04-11 11:25:26 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3001", // Replace with your frontend URL
+    origin: "http://localhost:3001", 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
@@ -21,7 +21,6 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use("/app", auth);
-// app.use("/app",auth);
 app.use("/api", list);
 const Port = 5000;
 app.listen(Port, () => {
